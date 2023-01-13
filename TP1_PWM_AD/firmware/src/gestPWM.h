a793fd3a2df78ad329e23b3941f7ed8ebd6bb488
Meven Ricchieri <74274472+Belprot@users.noreply.github.com>
2023-01-13 09:42:39 +0100

    Update comments

--- TP1_PWM_AD/firmware/src/gestPWM.h
+++ TP1_PWM_AD/firmware/src/gestPWM.h
@@ -4,9 +4,9 @@
 // GestPWM.h
 /*--------------------------------------------------------*/
 //	Description :	Gestion des PWM 
-//			        pour TP1 2016-2017
+//			        TP1 2022-2023
 //
-//	Auteur 		: 	C. HUBER
+//	Auteur 		: 	C. HUBER / MRI / SVE
 //
 //	Version		:	V1.1
 //	Compilateur	:	XC32 V1.42 + Harmony 1.08
@@ -18,7 +18,7 @@
 
 
 /*--------------------------------------------------------*/
-// Définition des fonctions prototypes
+// DÃ©finition des fonctions prototypes
 /*--------------------------------------------------------*/
 
 #define VAL_MAX_ADC 1023
@@ -29,22 +29,22 @@
 #define VAL_MAX_ANGLE 90
 #define VAL_MAX_ANGLEABS 180
 
-#define OFFSET_600US_SERVO 3000 // (35000 / 7) * 0.6 = 3000 // 3000 correspond à 600us (temps min impultion)
+#define OFFSET_600US_SERVO 3000 // (35000 / 7) * 0.6 = 3000 // 3000 correspond Ã  600us (temps min impultion)
 #define MULT_FACT_SERVO 50 // (((35000 / 7) * 2.4) - 3000) = 50  
 #define MULT_FACT_MOTOR 20 // 2000 / 100 = 20
 
 typedef struct {
-    uint8_t absSpeed;    // vitesse 0 à 99
-    uint8_t absAngle;    // Angle  0 à 180
-    int8_t SpeedSetting; // consigne vitesse -99 à +99
-    int8_t AngleSetting; // consigne angle  -90 à +90
+    uint8_t absSpeed;    // vitesse 0 Ã  99
+    uint8_t absAngle;    // Angle  0 Ã  180
+    int8_t SpeedSetting; // consigne vitesse -99 Ã  +99
+    int8_t AngleSetting; // consigne angle  -90 Ã  +90
 } S_pwmSettings;
 
 extern S_pwmSettings PWMData; 
 
 void GPWM_Initialize(S_pwmSettings *pData);
 
-// Ces 3 fonctions ont pour paramètre un pointeur sur la structure S_pwmSettings.
+// Ces 3 fonctions ont pour paramÃ¨tre un pointeur sur la structure S_pwmSettings.
 void GPWM_GetSettings(S_pwmSettings *pData);	// Obtention vitesse et angle
 void GPWM_DispSettings(S_pwmSettings *pData);	// Affichage
 void GPWM_ExecPWM(S_pwmSettings *pData);		// Execution PWM et gestion moteur.
